26e261c5c985000461cca195cc1bf727b729fe03
balparas321 <parasramola97299@gmail.com>
2022-05-29 09:06:16 +0530

    Checkpoint 2

---
+++ Checkpoint 2/src/screens/home/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import header from './common/header/Header'
+
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div> <header /> </div>
+    )
+  }
+}

dd9a2fab5dc613a7ff92e93052fbfbc0b5cd3f79
balparas321 <parasramola97299@gmail.com>
2022-05-29 11:50:24 +0530

    checkpoint 4

---
+++ Checkpoint 4/src/screens/home/Home.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react';
+
+import Header from '../../common/header/Header';
+import Movie from '../../common/header/Movie'
+class Home extends Component {
+  render() {
+    return (
+      <Header />
+    )
+  };
+}
+export default Home;
